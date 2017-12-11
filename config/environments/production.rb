fd6b21936c5ee3e51d00e251e7d72a43eb33e0f1
Richard Joe <rjoe19@hotmail.com>
2017-12-12 09:40:28 +1300

    undo previous commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
 
   # setups up paperclip to upload images to amazon s3
   config.paperclip_defaults = {
-    :storage => :s3-us-west-1,
+    :storage => :s3,
     :s3_credentials => {
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
